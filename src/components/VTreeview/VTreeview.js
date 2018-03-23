bd118bd79ea759e7444d32e17099aa28df34b710
Damjan Namjesnik <damjan@certissa.com>
2018-03-23 18:21:20 +0100

    selection implementation (end of part 2 of article series)

--- src/components/VTreeview/VTreeview.js
+++ src/components/VTreeview/VTreeview.js
@@ -1,4 +1,3 @@
-import '../../stylus/components/_input-groups.styl'
 import '../../stylus/components/_treeview.styl'
 
 import VTreeview from './VTreeview'
@@ -38,8 +37,38 @@ export default {
   },
 
   computed: {
+    allDescendantLeafs () {
+      let leafs = []
+      let searchTree = items => {
+        if (items[this.childrenField] !== undefined && items[this.childrenField].length > 0) {
+          items[this.childrenField].forEach(child => searchTree(child))
+        } else {
+          leafs.push(items)
+        }
+      }
+      searchTree(this.items)
+      return leafs
+    },
+    allDescendantLeafsSelected () {
+      return this.hasChildren && this.hasSelection && this.allDescendantLeafs.every(leaf => this.value.some(sel => sel === leaf))
+    },
     caption () {
       return this.items[this.captionField]
+    },
+    children () {
+      return this.hasChildren && this.items[this.childrenField]
+    },
+    hasChildren () {
+      return this.items[this.childrenField] !== undefined && this.items[this.childrenField].length > 0
+    },
+    hasSelection () {
+      return this.value !== undefined && this.value.length > 0
+    },
+    indeterminate () {
+      return this.hasSelection && this.hasChildren && this.someDescendantLeafsSelected
+    },
+    someDescendantLeafsSelected () {
+      return this.hasSelection && this.allDescendantLeafs.some(leaf => this.value.some(sel => sel === leaf))
     }
   },
 
@@ -47,21 +76,49 @@ export default {
     genRoot () {
       if (this.value) {
         return this.$createElement(VCheckbox, {
-          on: {
-            change: selection => {
-              this.$emit('input', selection)
-            }
-          },
           props: {
             label: this.caption,
-            'hide-details': true,
-            inputValue: this.value,
-            value: this.items
+            hideDetails: true,
+            // we set different behaviour if treeview has children or not
+            inputValue: this.hasChildren ? this.allDescendantLeafsSelected : this.value,
+            value: this.hasChildren ? this.allDescendantLeafsSelected : this.items,
+            indeterminate: this.indeterminate
+          },
+          on: {
+            change: selection => {
+              // we set different behaviour if treeview has children or not
+              if (this.hasChildren) {
+                if (this.allDescendantLeafsSelected) {
+                  // deselect all
+                  this.allDescendantLeafs.forEach(leaf => {
+                    let ix = this.value.indexOf(leaf)
+                    this.value.splice(ix, 1)
+                  })
+                } else {
+                  // select all
+                  this.allDescendantLeafs.forEach(leaf => {
+                    let ix = this.value.indexOf(leaf)
+                    if (ix === -1) {
+                      this.value.push(leaf)
+                    }
+                  })
+                }
+                this.$emit('input', this.value)
+              } else {
+                this.$emit('input', selection)
+              }
+            }
           }
-        }, [])
+        })
       } else {
         return this.$createElement('div', {
-          class: ['input-group', 'checkbox', 'input-group--hide-details', 'input-group--selection-controls', 'accent--text']
+          class: [
+            'input-group',
+            'checkbox',
+            'input-group--hide-details',
+            'input-group--selection-controls',
+            'accent--text'
+          ]
         }, [
           this.$createElement('label', {
             class: 'treeview-label'
@@ -87,8 +144,8 @@ export default {
     },
     genChildren () {
       let childElements = []
-      if (this.items[this.childrenField]) {
-        this.items[this.childrenField].forEach(child => {
+      if (this.hasChildren) {
+        this.children.forEach(child => {
           childElements.push(this.genChild(child))
         })
       }
